a3f4282dafc6aff6b22afb3914095555d03dde96
Luisa Cruz <luisa@kptncook.com>
2022-10-25 22:06:47 +0200

    Initial Commit

---
+++ MyPics/MyPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPicsApp.swift
+//  MyPics
+//
+//  Created by Shellflower on 25.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MyPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
